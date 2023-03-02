bf17f422188a95c80eff1d7009b8e03178e32c47
Adam Bako <bakoad@gmail.com>
2023-03-02 14:09:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ def read_df(file_name, index_col=None, date_col=None):
     return pd.read_csv(input_dir + file_name, index_col=index_col, parse_dates=date_col)
 
 
-df = read_df('analyzed')
+df = read_df('analyzed_output')
 
 st.table(df)
