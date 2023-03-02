5d8a04461a0007d21eb3ae52aceab8cf2c685ce0
Adam Bako <bakoad@gmail.com>
2023-03-02 14:02:51 +0100

    initial

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+input_dir = 'data/in/tables/'
+
+
+@st.cache_data
+def read_df(file_name, index_col=None, date_col=None):
+    return pd.read_csv(input_dir + file_name, index_col=index_col, parse_dates=date_col)
+
+
+df = read_df('analyzed')
+
+st.table(df)
